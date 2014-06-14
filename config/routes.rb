ceeec1766e8ab791a855faa7892c4b8e27a31b4b
ransolo <ransolo@me.com>
2014-06-13 23:05:34 -0500

    added dashboard view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'users#index'
+  root to: 'dashboard#index'
   resources :users
+  resources :dashboard, only: :index
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
