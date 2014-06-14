02aa86968e1c6d334ff536a3a420de2015103028
ransolo <ransolo@me.com>
2014-06-14 01:39:49 -0500

    refactor and style

--- app/controllers/users/songs_controller.rb
+++ app/controllers/users/songs_controller.rb
@@ -1,7 +1,6 @@
 class Users::SongsController < UsersController
+
   def show
     @song = Song.find(params[:id])
-    @song_comments = Comment.where(post_id: params[:id], post_class: "Song")
-    @recordings = @song.recordings
   end
 end
